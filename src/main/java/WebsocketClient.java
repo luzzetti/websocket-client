58c9b94be3a7ecfacf1d4d0209595f4074acbfbe
Christian <christian.luzzetti@gmail.com>
2021-05-21 00:11:02 +0200

    aggiunto impacchettamento tramite maven

--- src/main/java/WebsocketClient.java
+++ src/main/java/WebsocketClient.java
@@ -6,9 +6,19 @@ import java.util.Scanner;
 public class WebsocketClient {
 
     List<String> messaggi = new ArrayList<>();
-    private final static String INDIRIZZO_SERVER_CHAT = "";
+    private static String INDIRIZZO_SERVER_CHAT = "";
+    private static String NOME_UTENTE = "";
 
     public static void main(String[] args) throws Exception {
+
+        for (int i=0; i<args.length; i++) {
+            if (args[i].equals("-url")) {
+                INDIRIZZO_SERVER_CHAT = args[i+1].replace("\"", "");
+            }
+            if (args[i].equals("-nome")) {
+                NOME_UTENTE = args[i+1].replace("\"", "");
+            }
+        }
 
         final ChatClientEndpoint clientEndPoint =
                 new ChatClientEndpoint(new URI(INDIRIZZO_SERVER_CHAT));
@@ -18,7 +28,7 @@ public class WebsocketClient {
         while (!"ESCI".equals(msg)) {
             System.out.print("> ");
             msg = scanner.nextLine().trim();
-            clientEndPoint.sendMessage(msg);
+            clientEndPoint.sendMessage(NOME_UTENTE + ": " + msg);
         }
 
     }
